62bf73892d4fe314620dea4909fbbb8c070758d4
Chris Byrne <adapt0@adapt.gen.nz>
2022-02-23 09:00:01 +1300

    parse multipart body, decode base64

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const https = require('https');
 const SMTPServer = require("smtp-server").SMTPServer;
 const url = require('url');
 
+
 const urlWebhook = (() => {
     try {
         return url.parse(process.env.WEBHOOK);
@@ -18,6 +19,57 @@ const urlWebhook = (() => {
     }
 })();
 console.log(`Forwarding to ${urlWebhook.host}`);
+
+
+function parseBody(msg) {
+    // check for multipart message
+    const mParts = test.match(/.+/);
+    if (!mParts || !mParts[0].startsWith('--')) return msg;
+
+    const partsSplit = test.split(mParts[0])
+    if (partsSplit.length < 2) return msg;
+
+    // parse mutl parts
+    const multiParts = [];
+    for (let part of partsSplit) {
+        part = part.trimStart()
+        if ('--' === part) break;
+
+        const mHeaders = part.match(/^(?:.+(\r\n|\n))+\s+/);
+        if (!mHeaders) continue;
+
+        const headers = mHeaders[0];
+        const body = part.slice(headers.length).trimStart();
+        const kvs = Object.fromEntries(
+            headers.split('\n')
+                .map((h) => h.split(':', 2))
+                .filter(([k, v]) => k && v)
+                .map(([k, v]) => [k.trim().toLowerCase(), v.trim()])
+        );
+        multiParts.push({ headers: kvs, body });
+    }
+
+    // find part with desired content type
+    const findPartWithType = (desired) => {
+        return multiParts.find((p) => {
+            const contentType = p.headers['content-type'];
+            return contentType && contentType.includes(desired);
+        });
+    };
+
+    // prefer text/html (as text/plain is usually you need an HTML client)
+    let part = findPartWithType('text/html');
+    if (!part) part = findPartWithType('text/plain');
+    if (!part) part = multiParts[0];
+
+    // de-base64 as required
+    if (part.headers['content-transfer-encoding'] === 'base64') {
+        part = Buffer.from(part.body, 'base64').toString('utf-8');
+    }
+
+    return part;
+}
+
 
 function sendToWebHook(msg) {
     return new Promise((resolve, reject) => {
@@ -52,6 +104,7 @@ function sendToWebHook(msg) {
     });
 }
 
+
 const server = new SMTPServer({
     authOptional: true,
     onData(stream, session, callback) {
@@ -66,7 +119,7 @@ const server = new SMTPServer({
             // console.log({ headers, body });
 
             sendToWebHook(JSON.stringify({
-                content: body,
+                content: parseBody(body),
             })).then(() => {
                 callback();
             }).catch((e) => {
